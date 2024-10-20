bace1cd17db8f944bb660e113be19131b3d878e5
Frankily <frank.li.fl463@yale.edu>
2024-10-20 14:59:19 -0400

    email

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,13 @@ const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const nodemailer = require('nodemailer');
 const functions = require('firebase-functions');
+const cors = require('cors');
+
+const corsMiddleware = cors({
+  origin: 'https://www.praxim.ai',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+});
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
